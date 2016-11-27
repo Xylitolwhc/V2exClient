7b7e845c108253a16e503ecb3567bb04cfe25d6c
Xylitolwhc <xylitolwhc@gmail.com>
2016-11-27 18:59:26 +0800

    Complete DetailActivity

--- app/src/main/java/Adapters/ContentDetailAdapter.java
+++ app/src/main/java/Adapters/ContentDetailAdapter.java
@@ -1,6 +1,7 @@
 package Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -20,26 +21,54 @@ import whc.uniquestudio.v2exclient.R;
  */
 
 public class ContentDetailAdapter extends RecyclerView.Adapter<ContentDetailViewHolder> {
-
+    public static final int TYPE_HEADER = 0;
+    public static final int TYPE_NORMAL = 1;
+    private String title, content, username;
+    private Bitmap idImage;
     private Context context;
     private List<ContentDetail> contentDetailList;
 
-    public ContentDetailAdapter(Context context, List<ContentDetail> contentDetailList) {
-        this.context=context;
-        this.contentDetailList=contentDetailList;
+
+    public ContentDetailAdapter(Context context, List<ContentDetail> contentDetailList, String title, String content, String username,Bitmap idImage) {
+        this.context = context;
+        this.contentDetailList = contentDetailList;
+        this.title = title;
+        this.content = content;
+        this.username = username;
+        this.idImage=idImage;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return TYPE_HEADER;
+        } else return TYPE_NORMAL;
     }
 
     @Override
     public ContentDetailViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Log.d("Adapter",viewType+"");
-        ContentDetailViewHolder holder=new ContentDetailViewHolder(LayoutInflater.from(context).inflate(R.layout.detailcardview,parent,false));
-        return holder;
+        Log.d("Adapter", viewType + "");
+
+        if (viewType == TYPE_HEADER) {
+            return new ContentDetailViewHolder(LayoutInflater.from(context).inflate(R.layout.headcardview, parent, false), TYPE_HEADER);
+        } else {
+            return new ContentDetailViewHolder(LayoutInflater.from(context).inflate(R.layout.detailcardview, parent, false), TYPE_NORMAL);
+        }
     }
 
     @Override
     public void onBindViewHolder(ContentDetailViewHolder holder, int position) {
-        ContentDetail contentDetail=contentDetailList.get(position);
-        holder.contentDetailContent.setText(contentDetail.getString("replyContent"));
+        if (position == 0) {
+            holder.contentDetailTitle.setText(title);
+            holder.contentDetailContent.setText(content);
+            holder.contentDetailUsername.setText(username);
+            holder.contentDetailIdImage.setImageBitmap(idImage);
+        } else {
+            ContentDetail contentDetail = contentDetailList.get(position);
+            holder.contentDetailContent.setText(contentDetail.getString("replyContent"));
+            holder.contentDetailUsername.setText(contentDetail.getString("username"));
+            holder.contentDetailIdImage.setImageBitmap(contentDetail.getIdImage());
+        }
     }
 
     @Override
@@ -47,15 +76,26 @@ public class ContentDetailAdapter extends RecyclerView.Adapter<ContentDetailView
         return contentDetailList.size();
     }
 }
-    class ContentDetailViewHolder extends RecyclerView.ViewHolder{
-        protected TextView contentDetailUsername,contentDetailDetail,contentDetailContent;
-        protected ImageView contentDetailIdImage;
 
-        public ContentDetailViewHolder(View itemView) {
-            super(itemView);
-            contentDetailUsername=(TextView)itemView.findViewById(R.id.contentDetailUsername);
-            contentDetailDetail=(TextView)itemView.findViewById(R.id.contentDetailDetail);
-            contentDetailContent=(TextView)itemView.findViewById(R.id.contentDetailContent);
-            contentDetailIdImage=(ImageView)itemView.findViewById(R.id.contentDetailIdImage);
+class ContentDetailViewHolder extends RecyclerView.ViewHolder {
+    public static final int TYPE_HEADER = 0;
+    public static final int TYPE_NORMAL = 1;
+    protected TextView contentDetailUsername, contentDetailDetail, contentDetailContent, contentDetailTitle;
+    protected ImageView contentDetailIdImage;
+
+    public ContentDetailViewHolder(View itemView, int viewType) {
+        super(itemView);
+        if (viewType == TYPE_HEADER) {
+            contentDetailTitle = (TextView) itemView.findViewById(R.id.contentDetailTitleHead);
+            contentDetailUsername = (TextView) itemView.findViewById(R.id.contentDetailUsernameHead);
+            contentDetailDetail = (TextView) itemView.findViewById(R.id.contentDetailDetailHead);
+            contentDetailContent = (TextView) itemView.findViewById(R.id.contentDetailContentHead);
+            contentDetailIdImage = (ImageView) itemView.findViewById(R.id.contentDetailIdImageHead);
+        } else {
+            contentDetailUsername = (TextView) itemView.findViewById(R.id.contentDetailUsername);
+            contentDetailDetail = (TextView) itemView.findViewById(R.id.contentDetailDetail);
+            contentDetailContent = (TextView) itemView.findViewById(R.id.contentDetailContent);
+            contentDetailIdImage = (ImageView) itemView.findViewById(R.id.contentDetailIdImage);
         }
+    }
 }
--- app/src/main/java/whc/uniquestudio/v2exclient/ContentDetailActivity.java
+++ app/src/main/java/whc/uniquestudio/v2exclient/ContentDetailActivity.java
@@ -7,11 +7,13 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,10 +21,16 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import Adapters.ContentDetailAdapter;
+import FromGson.TheHottest;
 import FromJsoup.ContentDetail;
 
 /**
@@ -32,31 +40,41 @@ import FromJsoup.ContentDetail;
 public class ContentDetailActivity extends Activity {
     public static final int SHOW_RESPONSE = 0;
     public static final int CONNECT_FAILED = 1;
-    public static final int SHOW_PICTURE=2;
+    public static final int SHOW_PICTURE = 2;
 
     private String url, title, content, username;
     private RecyclerView contentDetailRecyclerView;
     private TextView contentDetailTitle, contentDetailUsername, contentDetailDetail, contentDetailContent;
-    private Bitmap idImage;
+    private Bitmap idImage=null;
     private ImageView contentDetailIdImage;
     private ContentDetailAdapter contentDetailAdapter;
-    private List<ContentDetail> contentDetailList=new ArrayList<>();
+    private List<ContentDetail> contentDetailList = new ArrayList<>();
+    private SwipeRefreshLayout swipeRefreshLayoutOfTheContent;
+
 
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
-                case SHOW_RESPONSE:{
-                    String s=(String)msg.obj;
-                    ((TextView)findViewById(R.id.testText2)).setText(s);
-                    contentDetailAdapter=new ContentDetailAdapter(ContentDetailActivity.this,contentDetailList);
-                    contentDetailRecyclerView.setAdapter(contentDetailAdapter);
+            switch (msg.what) {
+                case SHOW_RESPONSE: {
+                    String s = (String) msg.obj;
+                    ((TextView) findViewById(R.id.testText2)).setText(s);
+                    getPicture();
+                    contentDetailAdapter.notifyDataSetChanged();
+                    Toast.makeText(ContentDetailActivity.this, "刷新成功", Toast.LENGTH_SHORT).show();
                     break;
                 }
-                case CONNECT_FAILED:{
+                case CONNECT_FAILED: {
+                    Toast.makeText(ContentDetailActivity.this, "刷新失败", Toast.LENGTH_SHORT).show();
                     break;
                 }
+                case SHOW_PICTURE: {
+                    contentDetailAdapter.notifyDataSetChanged();
+                    break;
+                }
+
             }
+            swipeRefreshLayoutOfTheContent.setRefreshing(false);
         }
     };
 
@@ -66,9 +84,20 @@ public class ContentDetailActivity extends Activity {
         setContentView(R.layout.contentdetail);
 
         //doFindViewById();
-        //doGetIntent();
-        contentDetailRecyclerView=(RecyclerView)findViewById(R.id.contentDetailRecyclerView);
+        doGetIntent();
+        swipeRefreshLayoutOfTheContent = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayoutOfTheContent);
+        contentDetailRecyclerView = (RecyclerView) findViewById(R.id.contentDetailRecyclerView);
+
         contentDetailRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        contentDetailAdapter = new ContentDetailAdapter(ContentDetailActivity.this, contentDetailList, title, content, username, idImage);
+        contentDetailRecyclerView.setAdapter(contentDetailAdapter);
+        swipeRefreshLayoutOfTheContent.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                connectInternet(getIntent().getStringExtra("url"));
+            }
+        });
+        //connectInternet(getIntent().getStringExtra("url"));
         connectInternet(getIntent().getStringExtra("url"));
     }
 
@@ -81,24 +110,20 @@ public class ContentDetailActivity extends Activity {
 //        contentDetailIdImage = (ImageView) findViewById(R.id.contentDetailIdImage);
 //    }
 
-//    private void doGetIntent() {
-//        Intent intent = getIntent();
-//        if (intent.hasExtra("url")) {
-//            url = intent.getStringExtra("url");
-//            content = intent.getStringExtra("content");
-//            title = intent.getStringExtra("title");
-//            username = intent.getStringExtra("username");
-//            idImage = intent.getParcelableExtra("idImage");
-//
-//            contentDetailTitle.setText(title);
-//            contentDetailContent.setText(content);
-//            contentDetailUsername.setText(username);
-//            contentDetailIdImage.setImageBitmap(idImage);
-//            connectInternet(url);
-//        } else {
-//            finish();
-//        }
-//    }
+    private void doGetIntent() {
+        Intent intent = getIntent();
+        if (intent.hasExtra("url")) {
+            url = intent.getStringExtra("url");
+            content = intent.getStringExtra("content");
+            title = intent.getStringExtra("title");
+            username = intent.getStringExtra("username");
+            idImage = intent.getParcelableExtra("idImage");
+
+        } else {
+            finish();
+        }
+    }
+
 
     private void connectInternet(final String url) {
         new Thread(new Runnable() {
@@ -106,23 +131,82 @@ public class ContentDetailActivity extends Activity {
             public void run() {
                 try {
                     Document document = Jsoup.connect(url).get();
-                    Elements nodes=document.getElementsByClass("reply_content");
-                    Element userNames=document.getElementsByClass("box").get(1);
+                    Element userBox = document.getElementsByClass("box").get(1);
+                    Elements allUsers = userBox.getElementsByClass("cell");
 
                     StringBuffer stringBuffer = new StringBuffer();
-                    for (int i=0;i<nodes.size();i++){
-                        Element element2=nodes.get(i).select("a href").first();
-                        ContentDetail contentDetail=new ContentDetail();
-                        contentDetail.setString("replyContent",nodes.get(i).text().toString());
-                        contentDetailList.add(contentDetail);
-                        stringBuffer.append(nodes.get(i).text().toString()+"\n");
+                    //stringBuffer.append(userNames.toString());
+                    for (int i = 0; i < allUsers.size(); i++) {
+                        if (allUsers.get(i).id().toString().length() != 0) {
+                            Element user = allUsers.get(i);
+                            String imgUrl = user.select("img[src]").first().attr("src").toString();
+                            String replyContent = user.getElementsByClass("reply_content").first().text();
+                            String userName = user.select("a[href]").first().text();
+
+                            ContentDetail contentDetail = new ContentDetail();
+                            contentDetail.setString("replyContent", replyContent);
+                            contentDetail.setString("imageUrl", imgUrl);
+                            contentDetail.setString("username", userName);
+                            contentDetailList.add(contentDetail);
+                            stringBuffer.append(imgUrl + "\n" + userName + "\n" + replyContent + "\n");
+                        }
                     }
                     Message message = new Message();
                     message.what = SHOW_RESPONSE;
-                    message.obj=stringBuffer.toString();
+                    message.obj = stringBuffer.toString();
                     handler.sendMessage(message);
                 } catch (IOException e) {
                     e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void getPicture() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                for (int i = 0; i < contentDetailList.size(); i++) {//获取头像图片
+                    try {
+                        ContentDetail contentDetail = contentDetailList.get(i);
+                        URL avatar_miniUrl = new URL("http:" + contentDetail.getString("imageUrl"));
+                        connection = (HttpURLConnection) avatar_miniUrl.openConnection();
+                        connection.setRequestMethod("GET");
+                        connection.setConnectTimeout(8000);
+                        connection.setReadTimeout(8000);
+                        InputStream In = connection.getInputStream();
+                        Bitmap bitmap = BitmapFactory.decodeStream(In);
+                        contentDetail.setBitmap(bitmap);
+
+//                        URL avatar_normalUrl = new URL("http:" + theHottest.getString("avatar_normal") + ".png");
+//                        connection = (HttpURLConnection) avatar_normalUrl.openConnection();
+//                        connection.setRequestMethod("GET");
+//                        connection.setConnectTimeout(8000);
+//                        connection.setReadTimeout(8000);
+//                        InputStream avatar_normalIn = connection.getInputStream();
+//                        Bitmap avatar_normalBitmap = BitmapFactory.decodeStream(avatar_normalIn);
+//                        theHottest.setBitmap("avatar_normal", avatar_normalBitmap);
+//
+//                        URL avatar_largeUrl = new URL("http:" + theHottest.getString("avatar_large") + ".png");
+//                        connection = (HttpURLConnection) avatar_largeUrl.openConnection();
+//                        connection.setRequestMethod("GET");
+//                        connection.setConnectTimeout(8000);
+//                        connection.setReadTimeout(8000);
+//                        InputStream avatar_largeIn = connection.getInputStream();
+//                        Bitmap avatar_largeBitmap = BitmapFactory.decodeStream(avatar_largeIn);
+//                        theHottest.setBitmap("avatar_large", avatar_largeBitmap);
+                    } catch (ProtocolException e) {
+                        e.printStackTrace();
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        Message message = new Message();
+                        message.what = SHOW_PICTURE;
+                        handler.sendMessage(message);
+                    }
                 }
             }
         }).start();
